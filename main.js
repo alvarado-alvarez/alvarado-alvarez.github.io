8acba123c9ac8dc99d6757ee8a45f9ca5e78cb4d
Carlos Andrés Alvarado Álvarez <caalvara@mit.edu>
2024-05-11 21:38:08 -0400

    Blinking sun

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-const modeToggle = document.getElementById('mode-toggle');
-const body = document.body;
-
-modeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    header.classList.toggle('dark-mode');
-    a.classList.toggle('dark-mode');
-    a:visited.classList.toggle('dark-mode');
-});
+let onBeat = true;
+/**
+ * Switches the filename from filenameK to filename(1-K)
+ * 
+ * @param {string} svgname the name of svg to modify
+ * @param {string} filename1 valid file path, contains a unique 1 in the name
+ * @param {string} filename2 valid file path, contains a unique 2 in the name
+ */
+function blinkSun() {
+    svg = document.getElementById('Sun');
+    console.log('blinked!');
+        if (onBeat) {
+            svg.setAttribute('src','./SVGs/sunSprite2.svg');
+            onBeat = false;
+        }
+        else { 
+            svg.setAttribute('src','./SVGs/sunSprite1.svg');
+            onBeat = true;
+        };
+}
+setInterval(blinkSun, 400)
