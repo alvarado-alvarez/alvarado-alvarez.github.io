aaf8f150e660743ca4522028b60b94675e5768bf
Carlos Andrés Alvarado Álvarez <caalvara@mit.edu>
2024-05-11 22:22:44 -0400

    Successfully changes between dark and light mode

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+const body = document.body;
+
 let onBeat = true;
+let lightMode = true;
 /**
- * Switches the filename from filenameK to filename(1-K)
+ * Switches the filename from filename1 to filename2 if onBeat and filename2 to filename1 if not onBeat.
  * 
  * @param {string} svgname the name of svg to modify
  * @param {string} filename1 valid file path, contains a unique 1 in the name
@@ -9,6 +12,17 @@ let onBeat = true;
 function blinkSun() {
     svg = document.getElementById('Sun');
     console.log('blinked!');
+    if (lightMode) {
+        if (onBeat) {
+            svg.setAttribute('src','./SVGs/moonSprite2.svg');
+            onBeat = false;
+        }
+        else { 
+            svg.setAttribute('src','./SVGs/moonSprite1.svg');
+            onBeat = true;
+        }
+    }
+    else {
         if (onBeat) {
             svg.setAttribute('src','./SVGs/sunSprite2.svg');
             onBeat = false;
@@ -16,6 +30,16 @@ function blinkSun() {
         else { 
             svg.setAttribute('src','./SVGs/sunSprite1.svg');
             onBeat = true;
-        };
+        }
+    }
 }
 setInterval(blinkSun, 400)
+
+svg = document.getElementById('Sun')
+
+svg.addEventListener('click', () => {
+    console.log('clicked!');
+    lightMode = !lightMode;
+    blinkSun;
+    body.classList.toggle('dark-mode');
+})
