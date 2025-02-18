438b93744803807809cbb4843d7e695dcfa33e0f
Carlos Andrés Alvarado Álvarez <caalvara@mit.edu>
2025-02-17 22:06:30 -0500

    yuyuan styles

--- main.js
+++ main.js
@@ -3,5 +3,33 @@ const body = document.body;
 svg = document.getElementById('toggler');
     
 svg.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
+    toggleDarkMode();
 })
+
+// Function to enable dark mode
+function enableDarkMode() {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+  }
+
+  // Function to disable dark mode
+  function disableDarkMode() {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+  }
+
+  // Function to toggle dark mode
+  function toggleDarkMode() {
+    if (document.body.classList.contains("dark-mode")) {
+      disableDarkMode();
+    } else {
+      enableDarkMode();
+    }
+  }
+
+  // Apply the saved theme on page load
+  window.onload = function () {
+    if (localStorage.getItem("theme") === "dark") {
+      enableDarkMode();
+    }
+  };
